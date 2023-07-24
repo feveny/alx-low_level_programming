c9385fbdaaa6d1d37d34ba7128275a86f93c29f2
feveny <yfeven8@gmail.com>
2023-07-24 23:04:35 +0300

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strlen - Returns the length of a string
+ *@str: Input string
+ *Return: Length of string
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
