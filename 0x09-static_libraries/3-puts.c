8c153c2f3716586c86ae9d8a9cd07c63f17a4379
feveny <yfeven8@gmail.com>
2023-08-07 23:34:08 +0300

    error space

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -3,7 +3,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ *
+ * @s: char pointer variable
+ *
+ * Return: nothing
+ */
 void _puts(char *s)
 {
     puts(s);
--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -19,7 +19,6 @@ char *_strpbrk(char *s, char *accept)
 		}
 	s++;
 	}
-
-
+	
 return ('\0');
 }
