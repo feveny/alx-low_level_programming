aa64c13a079dab26dc37277a70bfc28ccbebc2a2
feveny <yfeven8@gmail.com>
2023-08-31 21:50:22 +0300

    edit done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,19 @@
 *Return: the converted number, or 0 if string b that is no 0 or 1, b is NULL
 *
 */
+
 unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int dec = 0;
+	int i;
+	unsigned int dec = 0;
 
-if (!b)
-return (0);
-for (i = 0; b[i]; i++)
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
 	{
-if (b[i] < '0' || b[i] > '1')
-return (0);
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
 		dec = 2 * dec + (b[i] - '0');
 	}
-retrn (dec);
+	return (dec);
 }
